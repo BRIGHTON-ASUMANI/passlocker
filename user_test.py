06d5d65bf28111d3d1529759570e1e91cb84a4b1
BRIGHTON-ASUMANI <asumanibrighton@gmail.com>
2018-08-23 23:37:53 +0300

    AttributeError: User object has no attribute delete_user

--- user_test.py
+++ user_test.py
@@ -57,6 +57,19 @@ class TestUser(unittest.TestCase):
             test_user.save_user()
             self.assertEqual(len(User.user_list),2)
 
+    # More tests above
+    def test_delete_user(self):
+            '''
+            test_delete_user to test if we can remove a user from our user list
+            '''
+            self.new_user.save_user()
+            test_user = User("Test","user","test@gmail.com","tested","pass","pass") # new user
+            test_user.save_user()
+
+            self.new_user.delete_user()# Deleting a user object
+            self.assertEqual(len(User.user_list),1)
+
+
 
 
 if __name__ == '__main__':
