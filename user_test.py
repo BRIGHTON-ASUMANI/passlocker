b4dd6728711cb9d5b510bfefe5ffc1f33b9feb0a
BRIGHTON-ASUMANI <asumanibrighton@gmail.com>
2018-08-23 22:37:24 +0300

    I created a save user test though i have not created the save contact function in the user.py module. THeerfore the test failed

--- user_test.py
+++ user_test.py
@@ -2,23 +2,27 @@ import unittest # Importing the unittest module
 from user import User # Importing the contact class
 
 class TestUser(unittest.TestCase):
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_user = User("Brighton","Asumani","bright@gmail.com","asu","password","password") # create contact object
 
-    '''
-    Test class that defines test cases for the contact class behaviours.
-    '''
+
     def test_init(self):
-    '''
-    test_init test case to test if the object is initialized properly
-    '''
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
 
-    self.assertEqual(self.new_user.first_name,"James")
-    self.assertEqual(self.new_user.last_name,"Muriuki")
-    self.assertEqual(self.new_user.email,"asumanibrighton@gmail.com")
-    self.assertEqual(self.new_user.username,"asu")
-    self.assertEqual(self.new_user.password,"newa$um0ney")
-    self.assertEqual(self.new_user.confirmpassword,"newa$um0ney")
+        self.assertEqual(self.new_user.first_name,"Brighton")
+        self.assertEqual(self.new_user.last_name,"Asumani")
+        self.assertEqual(self.new_user.email,"bright@gmail.com")
+        self.assertEqual(self.new_user.username,"asu")
+        self.assertEqual(self.new_user.password,"password")
+        self.assertEqual(self.new_user.confirmpassword,"password")
 
 
-
+if __name__ == '__main__':
+    unittest.main()
 if __name__ == '__main__':
     unittest.main()
